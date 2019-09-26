0e4a1d1e124a0a6567490c1e01bcdeccf580b7a8
Bertrand Atemkeng <ba@marketing-factory.de>
2019-09-26 20:53:14 +0200

    WIP

---
+++ src/Controller/QuizController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace QuizController;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuizController extends AbstractController
+{
+    /**
+     * @Route("/home", home)
+     */
+    public function home(){
+
+    }
+}
